02b00dec7c7f6a3fef5cc2a3cb81f800e08007b9
Andrei Kviatkouski <andruha-86@tut.by>
2021-01-10 12:22:05 +0300

    was added dependency junit-jupiter-api version 5.7.0

--- src/main/java/by/AndreiKviatkouski/Spider.java
+++ src/main/java/by/AndreiKviatkouski/Spider.java
@@ -1,144 +1,65 @@
 package by.AndreiKviatkouski;
 
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class Spider {
-    private static final int MAX_PAGES_TO_SEARCH = 10000;
 
+    private final Set<String> pagesVisited = new HashSet<>();
+    private  PrintWriter pw;
 
-    private Set<String> pagesVisited = new HashSet<String>();
-    private List<String> pagesToVisit = new LinkedList<String>();
-
-    private String currentUrl;
-
-
-//    private static final int MAX_DEPTH = 9;
-//    private HashSet<String> links2;
-
-//    public void WebCrawlerWithDepth() {
-//        links2 = new HashSet<String>();
-//    }
-//
-//    public void getPageLinks(String URL, int depth) {
-//        if ((!links2.contains(URL) && (depth < MAX_DEPTH))) {
-//            System.out.println(">> Depth: " + depth + " [" + URL + "]");
-//            try {
-//                links2.add(URL);
-//
-//                Document document = Jsoup.connect(URL).get();
-//                Elements linksOnPage = document.select("a[href]");
-//
-//                depth++;
-//                for (Element page : linksOnPage) {
-//                    getPageLinks(page.attr("abs:href"), depth);
-//                }
-//            } catch (IOException e) {
-//                System.err.println("For '" + URL + "': " + e.getMessage());
-//            }
-//        }
 
     /**
      * Our main launching point for the Spider's functionality. Internally it creates spider legs
      * that make an HTTP request and parse the response (the web page).
      *
-     * @param url        - The starting point of the spider
-     * @param searchWord - The word or string that you are searching for
+     * @param url   - The starting point of the spider
+     * @param words - The words or string that you are searching for
      */
 
-//    public void search(String url, ArrayList<String>words, int maxDepp) {
-//        int currentDepp = 0;
-//        while (this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {//1
-//            SpiderLeg leg = new SpiderLeg();
-//            if (this.pagesToVisit.isEmpty()) {
-//                currentUrl = url;
-//                this.pagesVisited.add(url);
-//            } else {
-//                currentUrl = this.nextUrl();
-//
-//            }
-//            leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in
-//            // SpiderLeg
-//            boolean success = leg.searchForWord(searchWord);
-//            if (success) {
-//                System.out.printf("**Success** Word %s found at %s%n", searchWord, currentUrl);
-//                break;
-//            }
-//
-//            if(this.currentDeep < maxDepp) {
-//                this.pagesToVisit.addAll(leg.getLinks());
-//                this.currentDeep = this.currentDeep + 1;
-//            }
-//        }
-//        System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
-//    }
-    public void searchRec(String url, List<String> words, int deep) {
-        SpiderLeg leg = new SpiderLeg();
-        if (deep < 8 &&
-                this.pagesVisited.size() < MAX_PAGES_TO_SEARCH &&
-                !this.pagesVisited.contains(url)) {// max deep
-//            if(this.pagesVisited.size() < MAX_PAGES_TO_SEARCH){
-//            if (!this.pagesVisited.contains(url)) {
-            leg.crawl(url);
-            List<String> links = leg.getLinks();// list link
-            Map<String, Integer> stat = new HashMap<>();// statistic count words
 
-            for (String world : words) {
-                int count = leg.countWorlds(world);
-                stat.put(world, count);
+    public void searchRecursive(String url, List<String> words, int MAX_DEEP, int MAX_PAGES_TO_SEARCH) {
+        SpiderLeg leg = new SpiderLeg();
+        int deepStart = 0;
+        if (deepStart < MAX_DEEP && this.pagesVisited.size() < MAX_PAGES_TO_SEARCH && !this.pagesVisited.contains(url)) {// start conditions
+            leg.crawl(url);
+            List<String> links = leg.getLinks();// create list link
+            Map<String, Integer> statistics = new HashMap<>();// statistic count words
+
+            for (String word : words) {
+                int count = leg.countWorlds(word);
+                statistics.put(word, count);
             }
 
             this.pagesVisited.add(url);// added link
 
-            this.printResult(url, words, stat); // print
+            this.printResult(url, words, statistics); // print
 
             for (String link : links) {
-                searchRec(link, words, deep + 1);
+                searchRecursive(link, words, deepStart + 1,MAX_PAGES_TO_SEARCH);//задаем глубину
             }
         }
     }
 
-
-    FileWriter objectName;
-    PrintWriter pw;
 
     public void printResult(String url, List<String> words, Map<String, Integer> stat) {
-        try {
-            //FileWriter objectName = new FileWriter("NewData2.csv",true);
-            //PrintWriter pw = new PrintWriter(objectName);
-            String builder = url + ",";
-            for (String key : words) {
-                Integer value = stat.get(key);
-                builder += value + ",";
-            }
-            builder += "\n";
 
-
-            pw.write(builder);
-            //pw.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        StringBuilder builder = new StringBuilder(url + " ");
+        for (String key : words) {
+            Integer value = stat.get(key);
+            builder.append(value).append(" ");
         }
+        builder.append("\n");
+        pw.write(builder.toString());
     }
 
-    public void openFile(String filename, boolean append) {
+
+    public void openFile(String file, boolean append) {
         try {
-            objectName = new FileWriter(filename, append);
-            pw = new PrintWriter(objectName);// add Row name url-1-2-3-4
+            FileWriter fw = new FileWriter(file, append);
+            pw = new PrintWriter(fw);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -146,38 +67,5 @@ public class Spider {
 
     public void closeFile() {
         pw.close();
-    }
-
-    /**
-     * Returns the next URL to visit (in the order that they were found). We also do a check to make
-     * sure this method doesn't return a URL that has already been visited.
-     *
-     * @return
-     */
-    private String nextUrl() {
-        String nextUrl;
-        do {
-            nextUrl = this.pagesToVisit.remove(0);
-        } while (this.pagesVisited.contains(nextUrl));
-        this.pagesVisited.add(nextUrl);
-        return nextUrl;
-    }
-
-
-    void writeResults() {
-//        String CSV_FILE = "C:\\test.csv";
-
-        try (
-                FileWriter CSV_FILE = new FileWriter("NewData.csv", true);
-                BufferedWriter writer = Files.newBufferedWriter(Paths.get(String.valueOf(CSV_FILE)));
-
-                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
-                        .withHeader("Linc", "Count"));
-        ) {
-            csvPrinter.printRecord(currentUrl, SpiderLeg.linksOnPage.size());
-            csvPrinter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/by/AndreiKviatkouski/SpiderLeg.java
+++ src/main/java/by/AndreiKviatkouski/SpiderLeg.java
@@ -7,18 +7,18 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpiderLeg {
     // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
     private static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
-    private List<String> links = new LinkedList<String>();
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.60 YaBrowser/20.12.0.963 Yowser/2.5 Safari/537.36";
+    private final List<String> links = new LinkedList<>();
     private Document htmlDocument;
     static Elements linksOnPage;
-
 
 
 //    }
@@ -35,8 +35,7 @@ public class SpiderLeg {
             Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
             Document htmlDocument = connection.get();
             this.htmlDocument = htmlDocument;
-            if (connection.response().statusCode() == 200) // 200 is the HTTP OK status code
-            // indicating that everything is great.
+            if (connection.response().statusCode() == 200) // 200 is the HTTP OK status code indicating that everything is great.
             {
                 System.out.println("\n**Visiting** Received web page at " + url);
             }
@@ -61,24 +60,30 @@ public class SpiderLeg {
      * Performs a search on the body of on the HTML document that is retrieved. This method should
      * only be called after a successful crawl.
      *
-     * @param searchWord - The word or string to look for
+     * @param word - The word or string to look for
      * @return whether or not the word was found
      */
-    public boolean searchForWord(String searchWord) {
-        // Defensive coding. This method should only be used after a successful crawl.
+
+
+    public int countWorlds(String word) {
+        int count = 0;
+        //   Defensive coding. This method should only be used after a successful crawl.
         if (this.htmlDocument == null) {
             System.out.println("ERROR! Call crawl() before performing analysis on the document");
-            return false;
+            return 0;
         }
-        System.out.println("Searching for the word " + searchWord + "...");
-        String bodyText = this.htmlDocument.body().text();
+        System.out.println("Searching for the word " + word + "...");
 
-        return bodyText.toLowerCase().contains(searchWord.toLowerCase());
-    }
+        String str = this.htmlDocument.body().text();
+        Pattern pattern = Pattern.compile(word);
+        Matcher matcher = pattern.matcher(str);
 
-    public int countWorlds(String world){
-        //TODO: add implementation
-        return 0;
+        while (matcher.find()) {
+            count++;
+        }
+
+        System.out.println(word + "   count: " + count);
+        return count;
     }
 
 
